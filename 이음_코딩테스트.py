a97f90d0a990ce7d57d8e13d7ddd5334a996596e
Aeius <psb6604@gmail.com>
2022-09-21 14:05:25 +0900

    0921

---
+++ 이음_코딩테스트.py
@@ -0,0 +1,27 @@
+'''배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+Ÿ array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다. Ÿ 배열을 정렬하면 [2, 3, 5, 6]입니다.
+Ÿ 배열의 3번째 숫자는 5입니다. 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때,
+commands의모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution함수를 작성해주세요. 
+
+제한사항
+• array의 길이는 1 이상 100 이하입니다. 
+• array의 각 원소는 1 이상 100 이하입니다. 
+• commands의 길이는 1 이상 50 이하입니다. 
+• commands의 각 원소는 길이가 3입니다. 입출력 예
+
+array                 commands                          return
+[1, 5, 2, 6, 3, 7, 4] [[2, 5, 3], [4, 4, 1], [1, 7, 3]] [5, 6, 3]
+
+'''
+# 정답
+def solution(array, commands):
+   answer = []
+   for idx, val in enumerate(commands):
+       arr = array[commands[idx][0] - 1: commands[idx][1]]
+       arr.sort()
+       answer.append(arr[val[2]-1])
+   return answer
+arr = [1, 5, 2, 6, 3, 7, 4]
+com = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(arr, com))
---
+++ 이음_코딩테스트2.py
@@ -0,0 +1,34 @@
+'''3명의 학생은 1번 문제부터 마지막 문제까지 다음과 같이 마킹합니다.
+1번 학생: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ... 
+2번 학생: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ... 
+3번 학생: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ... 
+
+1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때,
+가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+• 문제의 정답은 1, 2, 3, 4, 5중 하나입니다. 
+• 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요. '''
+
+def solution(answers):
+    answer = []
+    case1 = [1, 2, 3, 4, 5]
+    case2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    case3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    scores = [0, 0, 0]
+    for idx, val in enumerate(answers):
+        if case1[idx % len(case1)] == val:
+            scores[0] += 1
+        if case2[idx % len(case2)] == val:
+            scores[1] += 1
+        if case3[idx % len(case3)] == val:
+            scores[2] += 1
+    for idx, score in enumerate(scores):
+        if score == max(scores):
+            answer.append(idx+1)
+    return answer
+
+answer1 = [1,2,3,4,5]
+answer2 = [1,3,2,4,2]
+print(solution(answer1))
+print(solution(answer2))
