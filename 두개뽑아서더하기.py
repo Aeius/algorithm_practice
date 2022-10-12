555e9ae9e3ef84b7bd3f993a872a43a590d578e2
Aeius <psb6604@gmail.com>
2022-10-12 15:38:53 +0900

    1012

--- 두개뽑아서더하기.py
+++ 두개뽑아서더하기.py
@@ -42,3 +42,11 @@ def solution(numbers):
                     answer.sort()
     
     return answer
+
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    return sorted(list(set(answer)))
