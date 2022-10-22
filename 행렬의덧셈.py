40311f6aa081976e62cbebafb230e9f2f2dbb29f
Aeius <psb6604@gmail.com>
2022-10-22 12:34:44 +0900

    1022

--- 행렬의덧셈.py
+++ 행렬의덧셈.py
@@ -12,7 +12,6 @@ arr1	        arr2	        return
 
 
 def solution(arr1,arr2):
-    answer = [[]]
     for i in range(len(arr1)):
         for j in range(len(arr1[i])):
             arr1[i][j] += arr2[i][j]
